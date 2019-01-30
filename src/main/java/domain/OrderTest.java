2310f799e9870a52880c11e7b0c1842d2453420b
Koen Kamman <kammankoen@gmail.com>
2019-01-30 17:21:14 +0100

    Added .gitignore and test class for Order

---
+++ src/main/java/domain/OrderTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculatePrice() {
+    }
+}
